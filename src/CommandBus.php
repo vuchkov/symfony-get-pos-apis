935bbc7c1287bb7262c386916c82946d6727e50c
Ilcho Vuchkov <ilchovuchkov@gmail.com>
2024-07-18 06:15:37 +0300

    Create commandbus

---
+++ src/CommandBus.php
@@ -0,0 +1,35 @@
+<?php
+namespace App;
+
+//use App\CommandHandler\BarHandler;
+//use App\CommandHandler\FooHandler;
+use Psr\Container\ContainerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Contracts\Service\ServiceSubscriberInterface;
+
+class CommandBus implements ServiceSubscriberInterface
+{
+    public function __construct(
+        private ContainerInterface $locator,
+    ) {
+    }
+
+    public static function getSubscribedServices(): array
+    {
+        return [
+//            'App\FooCommand' => FooHandler::class,
+//            'App\BarCommand' => BarHandler::class,
+        ];
+    }
+
+    public function handle(Command $command): mixed
+    {
+        $commandClass = get_class($command);
+
+        if ($this->locator->has($commandClass)) {
+            $handler = $this->locator->get($commandClass);
+
+            return $handler->handle($command);
+        }
+    }
+}
