5596334c9d6595b9cec334a38fb03c3b37edea95
Ilcho Vuchkov <ilchovuchkov@gmail.com>
2024-07-18 18:57:44 +0300

    Add sqlite db, remove unused method in main controller

--- src/Helper/PosApisHelper.php
+++ src/Helper/PosApisHelper.php
@@ -55,25 +55,4 @@ class PosApisHelper
 
         return $content;
     }
-
-    /*public function loadV2()
-    {
-        $client = new GuzzleHttp\Client();
-        $res = $client->request('GET', 'https://api.github.com/user', [
-            'auth' => ['user', 'pass']
-        ]);
-        echo $res->getStatusCode();
-        // "200"
-        echo $res->getHeader('content-type')[0];
-        // 'application/json; charset=utf8'
-        echo $res->getBody();
-        // {"type":"User"...'
-
-        // Send an asynchronous request.
-        $request = new \GuzzleHttp\Psr7\Request('GET', 'http://httpbin.org');
-        $promise = $client->sendAsync($request)->then(function ($response) {
-            echo 'I completed! ' . $response->getBody();
-        });
-        $promise->wait();
-    }*/
 }
